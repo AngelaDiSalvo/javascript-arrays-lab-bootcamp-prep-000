352ab6acab46fb5bbad488da53d6dc28c0e2f21b
Angela DiSalvo <angela.disalvo@gmail.com>
2018-05-08 21:00:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = "I don't do much."
 function destructivelyAppendKitten(name) {
-  var kittens = ["Milo", "Otis", "Garfield"]
-  var name = ["Ralph"]
+  //var kittens = ["Milo", "Otis", "Garfield"]
+  //var name = ["Ralph"]
   kittens.push(name)
   return kittens
 }
